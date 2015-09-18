067077f39ff5eeea5973561d52ef6984344e03bb
Luke Nimtz <luke.nimtz@gmail.com>
2015-09-17 22:53:22 -0700

    deck sorts

--- index.js
+++ index.js
@@ -4,31 +4,76 @@ var suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts'];
 var ranks = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
 
 
+function ordered (a, b) {
+  // returns true if a comes before b in sorted deck, false if not
+  var aSuitIndex = suits.indexOf(a.suit);
+  var bSuitIndex = suits.indexOf(b.suit);
+  if (aSuitIndex < bSuitIndex) {
+    return true;
+  } else if (aSuitIndex > bSuitIndex) {
+    return false;
+  } else {
+    return ranks.indexOf(a.rank) < ranks.indexOf(b.rank);
+  }
+}
+
+function successive (a, b) {
+  // returns true if b comes directly after a in sorted deck
+  (ranks.indexOf(b.rank) - ranks.indexOf(a.rank) == 1) &&
+  (suits.indexOf(b.suit) - suits.indexOf(a.suit) == 1)
+}
+
+
+// I might put something like this in it's own module with it's own tests
+// if it didn't already exist on npm, in which case I would use the exisiting
+function qsort (arr) {
+  // recursive quicksort
+  if (arr.length === 0) return arr;
+  var testCard = arr.pop();
+  var left = qsort( arr.filter(function (card) { return ordered(card, testCard); }) );
+  var right = qsort( arr.filter(function (card) { return ordered(testCard, card); }) );
+  return left.concat(testCard, right);
+}
+
+
+
 function Deck () {
-	this.cards = []
-	var that = this
-	suits.forEach(function (suit) {
-		ranks.forEach(function (rank) {
-			that.cards.push({suit: suit, rank: rank})
-		})
-	})
+  this.cards = [];
+  var that = this;
+  suits.forEach(function (suit) {
+    ranks.forEach(function (rank) {
+      that.cards.push({suit: suit, rank: rank});
+    })
+  })
 }
 
 
 Deck.prototype.shuffle = function() {
-	var shuffled = []
+  var shuffled = [];
+  while (this.cards.length > 0){
+    shuffled.push( this.drawRandom() );
+  }
+  this.cards = shuffled;
+};
 
-	while	(this.cards.length > 0){
-		shuffled.push( this.drawRandom() );
-	}
 
-	this.cards = shuffled;
+Deck.prototype.isPracticallyShuffled = function() {
+  // returns false if at least 5 cards are still in original order
+  for (var i = 0; i < this.cards.length - 5; i++) {
+    this.cards.slice(i, 5)
+  };
 };
 
 
 Deck.prototype.drawRandom = function() {
-	return this.cards.splice( Math.floor( Math.random() * this.cards.length), 1)[0]
+  return this.cards.splice( Math.floor( Math.random() * this.cards.length), 1)[0];
 };
+
+
+Deck.prototype.sort = function() {
+  this.cards = qsort(this.cards.slice());
+};
+
 
 
 
--- test/test.js
+++ test/test.js
@@ -1,87 +1,45 @@
 'use strict';
 
-var should = require('should')
+var should = require('should');
 
-var Cards = require('../index.js');
+var Deck = require('../index.js').Deck;
 
-var testCards = [
-	{ suit: 'Spades', rank: 'Ace' },
-	{ suit: 'Spades', rank: '2' },
-	{ suit: 'Spades', rank: '3' },
-	{ suit: 'Spades', rank: '4' },
-	{ suit: 'Spades', rank: '5' },
-	{ suit: 'Spades', rank: '6' },
-	{ suit: 'Spades', rank: '7' },
-	{ suit: 'Spades', rank: '8' },
-	{ suit: 'Spades', rank: '9' },
-	{ suit: 'Spades', rank: '10' },
-	{ suit: 'Spades', rank: 'Jack' },
-	{ suit: 'Spades', rank: 'Queen' },
-	{ suit: 'Spades', rank: 'King' },
-	{ suit: 'Clubs', rank: 'Ace' },
-	{ suit: 'Clubs', rank: '2' },
-	{ suit: 'Clubs', rank: '3' },
-	{ suit: 'Clubs', rank: '4' },
-	{ suit: 'Clubs', rank: '5' },
-	{ suit: 'Clubs', rank: '6' },
-	{ suit: 'Clubs', rank: '7' },
-	{ suit: 'Clubs', rank: '8' },
-	{ suit: 'Clubs', rank: '9' },
-	{ suit: 'Clubs', rank: '10' },
-	{ suit: 'Clubs', rank: 'Jack' },
-	{ suit: 'Clubs', rank: 'Queen' },
-	{ suit: 'Clubs', rank: 'King' },
-	{ suit: 'Diamonds', rank: 'Ace' },
-	{ suit: 'Diamonds', rank: '2' },
-	{ suit: 'Diamonds', rank: '3' },
-	{ suit: 'Diamonds', rank: '4' },
-	{ suit: 'Diamonds', rank: '5' },
-	{ suit: 'Diamonds', rank: '6' },
-	{ suit: 'Diamonds', rank: '7' },
-	{ suit: 'Diamonds', rank: '8' },
-	{ suit: 'Diamonds', rank: '9' },
-	{ suit: 'Diamonds', rank: '10' },
-	{ suit: 'Diamonds', rank: 'Jack' },
-	{ suit: 'Diamonds', rank: 'Queen' },
-	{ suit: 'Diamonds', rank: 'King' },
-	{ suit: 'Hearts', rank: 'Ace' },
-	{ suit: 'Hearts', rank: '2' },
-	{ suit: 'Hearts', rank: '3' },
-	{ suit: 'Hearts', rank: '4' },
-	{ suit: 'Hearts', rank: '5' },
-	{ suit: 'Hearts', rank: '6' },
-	{ suit: 'Hearts', rank: '7' },
-	{ suit: 'Hearts', rank: '8' },
-	{ suit: 'Hearts', rank: '9' },
-	{ suit: 'Hearts', rank: '10' },
-	{ suit: 'Hearts', rank: 'Jack' },
-	{ suit: 'Hearts', rank: 'Queen' },
-	{ suit: 'Hearts', rank: 'King' }
-]
+var testDecks = require('./decks.js');
 
 describe('Deck', function () {
 
-	var deck;
+  var deck;
 
-	beforeEach(function () {
-		deck = new Cards.Deck();
-	})
+  beforeEach(function () {
+    deck = new Deck();
+  });
 
-	it('starts with the expected set of cards', function () {
-		deck.cards.should.eql(testCards)
-	})
+  it('starts with the expected set of cards', function () {
+    deck.cards.should.eql(testDecks.sorted);
+  });
 
-	describe('shuffle()', function () {
+  describe('shuffle()', function () {
 
-		it('shuffles the cards', function () {
-			deck.shuffle()
-			console.log(deck.cards)
-		})
+    it('shuffles the cards', function () {
+      deck.shuffle();
+      // console.log(deck.cards) // wow this is actually impossible to test...
+      deck.cards.should.not.eql(testDecks.shuffled); // there is an astronomical chance that even this will incorrectly fail
+    })
 
-	})
+  });
 
-	
-})
+  describe('sort()', function() {
+    
+    it('sorts the cards', function () {
+      deck.cards = testDecks.shuffled.slice();
+      deck.sort();
+      deck.cards.should.eql(testDecks.sorted);
+    });
+
+  });
+
+  
+});
 
 
 
